802c05a76e157b24e72b1b56b4c4c497fc39f648
FelipeSena02 <felipessena02@gmail.com>
2025-05-05 11:52:40 -0300

    Adicionando cliente

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const usuarioRoutes = require('../bem-te-vi/routes/usuarioRoutes');
+const clienteRoutes = require('../bem-te-vi/routes/clienteRoutes');
 
 app.use(express.json()); 
 app.use('/api', usuarioRoutes);
+app.use('/api', clienteRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
---
+++ routes/clienteRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const clienteController = require('../controllers/clienteController');
+
+// Definição das rotas
+router.get('/clientes', clienteController.getAllClientes);
+router.post('/clientes', clienteController.createCliente);
+router.get('/clientes/:id', clienteController.getClienteById);
+router.put('/clientes/:id', clienteController.updateCliente);
+router.delete('/clientes/:id', clienteController.deleteCliente);
+
+module.exports = router;
