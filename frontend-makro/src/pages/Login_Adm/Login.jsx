2a197885cc9c719852b1436d953ee3ebd7a935ab
Layssa Oliveira <layssaoliveira626@gmail.com>
2025-05-06 19:54:48 -0300

    restaurando arquivos e subindo para origin

--- frontend-makro/src/pages/Login_Adm/Login.jsx
+++ frontend-makro/src/pages/Login_Adm/Login.jsx
@@ -1,25 +1,12 @@
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 import './login.css';
 import Logo from '../../assets/logo_makro.png';
-import api from '../../services/api'
 
 function Login() {
-    const navigate = useNavigate(); 
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-    const [erro, setErro] = useState('');
+    const navigate = useNavigate(); // Hook para navegação
 
-    const handleLogin = async() => {
-        try {
-            const response = await api.post('/login', { email, senha })
-            if(response.status == 200) {
-                navigate('/solucoes_adm'); // Redireciona para a tela de soluções
-            }
-        } catch(error) {
-            setErro('E-mail ou senha inválidos')
-        }
-        
+    const handleLogin = () => {
+        navigate('/solucoes_adm'); // Redireciona para a tela de soluções
     };
 
     return (
@@ -30,24 +17,11 @@ function Login() {
             <div className='bloco_login'>
                 <h2>Login</h2>
                 <div className='input_group'>
-                    <input 
-                    type='email' 
-                    placeholder='E-mail' 
-                    value={email}
-                    onChange ={e => setEmail(e.target.value)}
-                    className='input_letra_branca'
-                    />
+                    <input type='email' placeholder='E-mail' />
                 </div>
                 <div className='input_group'>
-                    <input 
-                    type='password' 
-                    placeholder='Senha' 
-                    value={senha}
-                    onChange={e => setSenha(e.target.value)}
-                    className="input_letra_branca"
-                    />
+                    <input type='password' placeholder='Senha' />
                 </div>
-                {erro && <p style={{ color: 'red' }}>{erro}</p>}
                 <button className='enter_btn' onClick={handleLogin}>Entrar</button>
             </div>
         </div>
